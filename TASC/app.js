36a4ad9aae4161c82e459d1874a2855d08d4ccfa
Ahmad <ahmadsattar.7867@gmail.com>
2024-04-26 16:00:57 +0500

    a

---
+++ TASC/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const fs = require('fs'); 
+const signinRoute = require("./Express/Routes/signin");
+const indexRoute = require("./Express/Routes/index");
+
+
+const app = express();
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+//defining routes
+app.use('/signin', signinRoute);
+app.use('/index', indexRoute);
+
+
+
+// to serve main css file
+app.use(express.static(path.join(__dirname, '', '')));
+//to serve the signin css file
+app.use(express.static(path.join(__dirname, 'Express', 'views')));
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
