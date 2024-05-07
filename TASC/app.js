16ef02b8b07912fbff7fc52ae1e4f9b97acb5e5d
Ahmad <ahmadsattar.7867@gmail.com>
2024-05-07 19:31:37 +0500

    p

--- TASC/app.js
+++ TASC/app.js
@@ -1,27 +1,38 @@
 const express = require("express");
 const path = require("path");
 const fs = require('fs'); 
-const signinRoute = require("./Express/Controller/signin");
-const indexRoute = require("./Express/Routes/index");
-const signupRoute = require("./Express/Controller/signup");
+const mongoose = require('mongoose');
+const authRoutes = require('./Express/Routes/authRoutes');
+const exp = require("constants");
 
 
 const app = express();
 const hostname = '127.0.0.1';
 const port = 3000;
 
+//Middleware
+app.use(express.json()  );
+
+//setting up view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'Express', 'views'));
+
+
+const dbURI = 'mongodb+srv://f219284:ahmad7867@cluster0.ajlxhg3.mongodb.net/node-auth';
+mongoose.connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 
 //defining routes
-app.use('/signup', signupRoute);
-app.use('/signin', signinRoute);
-app.use('/index', indexRoute);
-
+app.use(authRoutes);
 
 
 // to serve main css file
 app.use(express.static(path.join(__dirname, '', '')));
 //to serve the signin css file
 app.use(express.static(path.join(__dirname, 'Express', 'views')));
+// app.use(express.static('public'));
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
